789399e1c1a564c077396b8e1021958164b82df4
Jerrynjoku <108212426+Jerrynjoku@users.noreply.github.com>
2023-07-16 19:40:39 +0100

    Update and rename index.html to  form.php

---
+++ form.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Login Form</title>
+</head>
+
+<body>
+<form method="post" action="login.php">
+Username: <input type="text" name="username" />
+
+Password: <input type="password" name="password" />
+
+<input type="submit" value="Login" />
+
+</form>
+</body>
+</html>

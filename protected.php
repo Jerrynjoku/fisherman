10c9bf9940d71c16116e31ea163893a3643d0e58
Jerrynjoku <108212426+Jerrynjoku@users.noreply.github.com>
2023-07-16 19:59:25 +0100

    Update and rename signup_style.css to protected.php

---
+++ protected.php
@@ -0,0 +1,14 @@
+<?php
+
+include "auth.inc";
+
+?>
+<html>
+<head>
+<title>Protected Page </title>
+</head>
+
+<body>
+Content in here will only be shown if the username and password supplied are correct.
+</body>
+</html>
